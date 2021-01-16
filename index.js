4a0cf0c3b252f609c95bb1aed39c070472a2ea24
Raveen K Sharma <raveen08@gmail.com>
2021-01-16 21:50:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,30 +1,55 @@
+<<<<<<< HEAD
 
 function wakeDog(dogName, dogBreed) {
+=======
+<<<<<<< HEAD
+const wakeDog = function (dogName, dogBreed) {
+>>>>>>> d43bcc4bea2b1c33a2d863b9bae2101e65ddfe75
   console.log(`Wake ${dogName} the ${dogBreed}`);
   return `Wake ${dogName} the ${dogBreed}`
 }
 
+<<<<<<< HEAD
 function leashDog(dogName="Byron", dogBreed="poodle") {
+=======
+const leashDog = function(dogName="Byron", dogBreed="poodle") {
+>>>>>>> d43bcc4bea2b1c33a2d863b9bae2101e65ddfe75
   console.log(`Leash ${dogName} the ${dogBreed}`);
   return `Leash ${dogName} the ${dogBreed}`
 }
 
+<<<<<<< HEAD
 function walkToPark(dogName="Byron", dogBreed="poodle") {
+=======
+const walkToPark = function(dogName="Byron", dogBreed="poodle") {
+>>>>>>> d43bcc4bea2b1c33a2d863b9bae2101e65ddfe75
   console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
   return `Walk to the park with ${dogName} the ${dogBreed}`
 }
 
+<<<<<<< HEAD
 function throwFrisbee(dogName="Byron", dogBreed="poodle") {
+=======
+const throwFrisbee = function(dogName="Byron", dogBreed="poodle") {
+>>>>>>> d43bcc4bea2b1c33a2d863b9bae2101e65ddfe75
   console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
   return `Throw the frisbee for ${dogName} the ${dogBreed}`
 }
 
+<<<<<<< HEAD
 function walkHome(dogName="Byron", dogBreed="poodle") {
+=======
+const walkHome = function(dogName="Byron", dogBreed="poodle") {
+>>>>>>> d43bcc4bea2b1c33a2d863b9bae2101e65ddfe75
   console.log(`Walk home with ${dogName} the ${dogBreed}`);
   return `Walk home with ${dogName} the ${dogBreed}`
 }
 
+<<<<<<< HEAD
 function unleashDog(dogName="Byron", dogBreed="poodle") {
+=======
+const unleashDog = function(dogName="Byron", dogBreed="poodle") {
+>>>>>>> d43bcc4bea2b1c33a2d863b9bae2101e65ddfe75
   console.log(`Unleash ${dogName} the ${dogBreed}`);
   return `Unleash ${dogName} the ${dogBreed}`
 }
@@ -32,5 +57,9 @@ function unleashDog(dogName="Byron", dogBreed="poodle") {
 const routine = [wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog]
 
 function exerciseDog(dog, breed) {
+<<<<<<< HEAD
   return routine.map(fn => fn(dog, breed))
 }
+=======
+  return routine.map(fn => fn(dog, breed))
+>>>>>>> d43bcc4bea2b1c33a2d863b9bae2101e65ddfe75
